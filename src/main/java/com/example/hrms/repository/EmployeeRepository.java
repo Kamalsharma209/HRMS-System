88e943f9480599bd723c4521fd4bde36b22cd6b8
Your Name <Kamalsharma4769@gmail.com>
2026-06-14 23:42:32 +0530

    repo changes

--- src/main/java/com/example/hrms/repository/EmployeeRepository.java
+++ src/main/java/com/example/hrms/repository/EmployeeRepository.java
@@ -3,12 +3,16 @@ package com.example.hrms.repository;
 import com.example.hrms.module.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.List;
 
-public interface EmployeeRepository
-        extends MongoRepository<Employee, String> {
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
 
     Optional<Employee> findByEmpCode(String empCode);
+
+    Optional<Employee> findByUsername(String username);
+
+    Optional<Employee> findByEmail(String email);
+
     List<Employee> findByActive(boolean active);
 }
