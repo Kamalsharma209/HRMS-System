7952a427986dbe09873d9b02f3e0f40ec86d91a0
Your Name <Kamalsharma4769@gmail.com>
2026-06-24 23:55:01 +0530

    reducing bugs

--- src/main/java/com/example/hrms/controller/EmployeeController.java
+++ src/main/java/com/example/hrms/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import com.example.hrms.service.EmailProducer;
 import com.example.hrms.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
